274c12528f507762819cc46ca6d4c6475d946edb
Liviu Marian Mazilu <liviumazilu79@gmail.com>
2022-05-23 19:25:35 +0200

    Implemented Put, Delete and Get DB methods.

--- src/main/java/com/stivalePrimaverile/SpringApp/student/StudentController.java
+++ src/main/java/com/stivalePrimaverile/SpringApp/student/StudentController.java
@@ -27,4 +27,19 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Long studentId) {
+        return studentService.getStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestBody Student student) {
+        studentService.updateStudent(studentId, student);
+    }
 }
--- src/main/java/com/stivalePrimaverile/SpringApp/student/StudentService.java
+++ src/main/java/com/stivalePrimaverile/SpringApp/student/StudentService.java
@@ -3,6 +3,7 @@ package com.stivalePrimaverile.SpringApp.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -23,8 +24,29 @@ public class StudentService {
     public void addNewStudent(Student student) {
         Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
         if (studentOptional.isPresent()) {
-            throw new IllegalArgumentException("Student with this email already exists");
+            throw new IllegalArgumentException("Student with email" + student.getEmail() + " already exists");
         }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalArgumentException("deleteStudent: Student with id " + studentId + " not found");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    public Student getStudent(Long studentId) {
+        return studentRepository.findById(studentId).orElseThrow(() -> new IllegalArgumentException("getById: Student with id " + studentId + " not found"));
+    }
+
+    public void updateStudent(Long studentId, Student student) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalArgumentException("updateStudent: Student with id " + studentId + " not found");
+        }
+        student.setId(studentId);
         studentRepository.save(student);
     }
 }
